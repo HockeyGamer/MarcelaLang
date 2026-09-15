d0a0db27d4f058d957f3ea53446677e299c3284d
HockeyGamer <conorhockey12@gmail.com>
2026-09-15 16:30:15 -0600

    initial parser functionality

--- parser.c
+++ parser.c
@@ -4,5 +4,22 @@
 
 void parse(const TokenList* list) {
     printf("Parsing %d tokens\n", list->count);
-    return;
+    
+    for(int i = 0; i < list->count; i++) {
+        const Token* token = &list->tokens[i];
+        
+        const Token* nextToken = (i + 1 < list->count) ? &list->tokens[i + 1] : NULL;
+        if(token->type == TOKEN_NUMBER) {
+            printf("parsed number: %d\n", token->value);
+        } else if (token->type == TOKEN_PLUS) {
+            printf("parsed plus\n");
+
+            if (nextToken && nextToken->type == TOKEN_PLUS){
+                fprintf(stderr, "Error: Unexpected token after plus\n");
+                return;
+            }
+        } else if (token->type == TOKEN_EOF) {
+            printf("parsed EOF\n");
+        }
+    }
 }
